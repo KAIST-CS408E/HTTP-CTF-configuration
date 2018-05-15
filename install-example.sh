d21cb34586fa1307d40caf4929d9a5180ad92bb2
Hobin Ryu <hobincar@kaist.ac.kr>
2018-05-15 18:16:41 +0900

    Update install-example.sh

--- install-example.sh
+++ install-example.sh
@@ -35,11 +35,11 @@ pip install -r \$VAGRANT_HOME/HTTP-CTF/dashboard/requirements.txt
 
 tee \$VAGRANT_HOME/HTTP-CTF/container-creator/example.json << END2
 {
-    "num_services": 5,
-    "name": "Awesome CTF",
-    "services": ["driller","poipoi","sillybox","tattletale","temperature"],
+    "num_services": 3,
+    "name": "CTF-example",
+    "services": ["poipoi","sillybox","tattletale"],
     "sudo": true,
-    "teams": [{"name":"team1","namespace":"team1"},{"name":"team2","namespace":"team2"},{"name":"team3","namespace":"team3"},{"name":"team4","namespace":"team4"}],
+    "teams": [{"name":"team1","namespace":"team1"},{"name":"team2","namespace":"team2"},{"name":"team3","namespace":"team3"},{"name":"team4","namespace":"team4"},{"name":"team5","namespace":"team5"}],
     "flag_storage_folder": "/flags",
     "containers_host": "127.0.0.1",
     "containers_ports_start" : 10000
@@ -49,37 +49,37 @@ END2
 tee \$VAGRANT_HOME/HTTP-CTF/dashboard/config/firebaseConfig.json << END2
 {
   "type": "service_account",
-  "project_id": "http-1661e",
-  "private_key_id": "8932b53516590f13928ad4501177f953ed86d8ff",
-  "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQDBQTDKmR8YiPEd\nMM2K3nRBdjsydhwemrOcSfRsInX6YPe0qnoSuGoXRRmafCGk1rXg5MbOummuw3Ou\nPFp3pizHuLK55SqOP6LhjhGOF7vswQpKX+ncnuziLmft3IVMf04ICJuK+Y24Lt6m\n9OnlgnWOwqN24KGEpXTsfcl4/cKaKKZJhuJ2tMf0ChxHhR/EKFOFnN4/hRfTROiX\nBGK2RQSutIUDG2YT1idKWJCR3P/P2C6z0ZIp2qtUDrFwUBXPSnCY5PFAJLj9stFS\nhukXZZxlculzBCsNI7mri3kxVZvpgFhoMK1XvgfKx6lkhD5yoeK0ay6zv1WVpbY6\nEx6X+Z3JAgMBAAECggEABLA+J2ojK/NAO0EF+Y33pCdtcfuAJ49sAa6LInehulpx\neVWkoBvkf1jxhog8q40Qd6AWln5wBi8EeVLfmdUYnhtOuGMVNFiE9858gfX03Oka\n2hlSg9lWCKTk0quFud3W9iuu9LEPesCQrbGrKNfl3iSTdpS7omt+XHxEP9N940Nu\nl06FPobulpwpbBq5KUdaW+3L9GJB+Vk9fk4My5OZK8ImyLBW/GvQcVjvWQTF7b/n\n1qqzNLv7dYv5DxO477iVDrZlQlWlOEItkBydIMXkfUv7mfPiVIBR2y3BaBOF/0NU\nM4x1VBQOEVs2VI8I6Q1b5RNc9ubp3bnWVipce0RAMQKBgQD456NBtLibgWYbDRD/\nOsqq4S5su98fXDiU4lSLvoZNB9ymMP3wfXK38lt10AgQkPszzJc+6ibgdCwtT1fh\ne58vcOoAg8/R9oPdEq9r6OfoGAQkjIcFP36UMmsW6qcPb1qk5CXdl3qAfKtGKdov\nMPm0Z5kSB8cM4Jq8u/fTEFCaEQKBgQDGw3NGUNw27v6RaR+MDsfpA7l8gfhxuqUQ\nbgDD0hemNjz/kK2Miwkp2uWBVAzTvGXHIibGtDXG/gE2PRZyBDGgadx5ROJJq09R\nsjfNP7dBwNWw/g1qHfcYyzeYjpkD4yycPGZ//cMBVPLxvzYJT9l/tUggUaLB90XM\n6ZHEtcJQOQKBgQCAgL6VjqL8jdrUPnO3AXhernEUbA4ul6MSoqL5nryhilyNZJ/G\nCE4wz6wOb/+p5d/7BzZSofq+SNwB7IO3guZVTNtStrqVcMr1WM8+S/lmpveEkxSm\nTfigtEL7itwDKP9IQA2YkLsLGNIoIDVnxcY1MIMjfuZyLIdWx0iULEkgsQKBgEKt\nVGFVRNqEyA89paCVYMixQCsdapu0UiU10bnfI6HVdhcBkMah6ZwJ243MBVWOUNJB\niaKE17l+gieQzJPJBmDawmRIEGVIO56bW47V/gLnzofGYfekP70ohuj9hSVBszhi\nJrxvA1jSB4HDdDwhdkQaKyz0VdYdvmOkQwsrMhk5AoGACYqRxCAomKgTV++1gfug\nhq5DBVwZJSQ01EgrasDO+ZojlTskYgBePwz5exO7KhUyIgg/alVshMMxr/BH2jeM\ngZwlqvbtuVCOLgXaT84tRgAwHu8ssgo1silEBOu/FU3jSqpVxyfFcwtmSNCk+otR\nWS5kk3Buf1ywotBFRmko4ug=\n-----END PRIVATE KEY-----\n",
-  "client_email": "firebase-adminsdk-ryz10@http-1661e.iam.gserviceaccount.com",
-  "client_id": "112948171011989343622",
+  "project_id": "cs408e-http",
+  "private_key_id": "fcec7bee57a5b4aeabf611a44ff0093e8639c09f",
+  "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQC+dGjY03RU62c0\n8OR2+9t7x7R+8If1D3GCjyrIZRgvMAKId0e2/8dTblP6TIBO0liCiiuWJKc2l143\nd8BH5VOHVWeefdkqQpyd+1H/VyBsEVCQNEFH+ZUkaARqzk+qsuUx0pVmSotblQcF\n4N+dEaad3aRIS8OREraE7BzafxbuCUHDSo0XDmW1nGExQ+oXswTcMix5H+GbeDTw\nRxrzDocqHhNs5qI7EtC8X2nUHp74D6EiNJ5c35r+fAcXzUFvY/HyXk8xxsMgHGyD\nVsVLJX+aqbTGe79Q/161atXlKuAyyfyBTia1wkRbRtchTRGw/YUkhfJ9t9ohW/Rk\nNUhFFxavAgMBAAECggEAERcPZ3wwtq65qqRTzqIoCPybzj5APPXJ+O4RkpI7xX7j\nOKKszUI40obrwzdFK42FgrqU/FqapJ6YRkBD1qUV8JFx2bovmAgKghn7ZX8dz4S4\nPCXBloynXPEhNa0AtMUWevjguMvx+GGwPnn2JuPewHtokqeLINlTMSyiJDB+UBVd\n3n8s8mZ6jOwr6w/kbloo5qCKv25zFP0lW562u9FzNBAQa8pONfOEYmZJ4kLKKk3s\nC5FOZwMBLCizT/GIpDVKXh1vrjwoiCmuHF0SUZIYre6oBizGY4pKvjc0SGLhRk6a\nlN+rn3GpR+AkplI27G+MynS1TbQYujM0B2f1I0LXxQKBgQDqK0/oBxuWuzE222LE\nfKIWi+zRAJ35/tIFB0zAjSLnifJTOJipJEJSEQ/AJLpdcKMNk5pmhEUSHrgjd1uX\nB1t/JTaZkO9JYxNGO7fKMeHcfX3KajFFo8/h8r11bEjYCpuV7lAbqtqeaINfWsen\njcjKa1//280hqe2maUna3ZNybQKBgQDQNc6SZC9GioMKvHKFEP0C+J2x5Vsh37iM\nq7vOGlAqTvOjxS5kG1TuZyuFAgVYuEUHDZqiDGrJkaOvPsCIh05QEyeWeWbzPe4G\nYHC+JDkoMeoA+Z8gVFSSJgK2cUW7sYU2zo+nweIjlcqXZS07PZZJnUJeoU311wVx\nQA4a9+pcCwKBgBBErAt5mvAQ1y1xdl4iLnyUggJPIaqBioOw6yMhawBc9AnWD70I\neNcMumRl6NCfB6Tk0UA4BjhpC+/i1ekbKL8fjbiDeixGBsvFUFJqvr87tKaAVCX7\naHDRFVp4CGOB8ScsZEJXz1Jb/mx8eQxp7D4r0YKL+adSD+GUxP3AkQLNAoGAGLKp\nRFg5+2Ym0Tk1ZG91c8rF+fo46zW8kIM2jeY1KC+hWwXi0OElG/qFAQwSUknZdlK4\nywwqBqsN/ZW2BPv34CvFgGX6PlRUTdFWzldBqHDzTxZNN+qI9aUooTeii/cs1CmA\nhOfMyWfVVkwcJ3DQyP6shskflE/jR2HPyocp960CgYEA3sAUO2u7y8PqiWXshBzg\nBylTxKWFLWVn7gqJPWNXUVR2x0VAuH+nFNX4tLCrpyeNOhziS+vt2fIpvoX0u0ld\n58+CVZMIMAlRdn1gnGzTXa3PAfafW9jBWP+eoIrDH6/sE0+KF+9nUQNtaYaRfifD\nnbbfjkJipOTxXy9/kBfVJdQ=\n-----END PRIVATE KEY-----\n",
+  "client_email": "firebase-adminsdk-8cfa6@cs408e-http.iam.gserviceaccount.com",
+  "client_id": "113214710667042764687",
   "auth_uri": "https://accounts.google.com/o/oauth2/auth",
   "token_uri": "https://accounts.google.com/o/oauth2/token",
   "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-ryz10%40http-1661e.iam.gserviceaccount.com"
+  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-8cfa6%40cs408e-http.iam.gserviceaccount.com"
 }
 
 END2
 
 tee \$VAGRANT_HOME/HTTP-CTF/dashboard/config/teamConfig.json << END2
 {
-    "api_secret": "YOUKNOWSOMETHINGYOUSUCK",
+    "api_secret": "kkosraclkkd4kb2g",
     "name": "CS408(E)_HTTP_CTF",
     "api_base_url": "http://127.0.0.1:4000",
-    "teams": {"0":{"name":"team1","hashed_password":"w4ex4a6zz5xe2hp0"},"1":{"name":"team2","hashed_password":"wkjtzeuk2bgzslhr"},"2":{"name":"team3","hashed_password":"d9ov9uwf6k3bnxqy"},"3":{"name":"team4","hashed_password":"7wjusqda94nvjro6"}}
+    "teams": {"0":{"name":"team1","hashed_password":"hgltzitmee7kg281"},"1":{"name":"team2","hashed_password":"39msu1bd6j290hmu"},"2":{"name":"team3","hashed_password":"ourac0z55zprxo0k"},"3":{"name":"team4","hashed_password":"c6p9czzn4iyzkxr3"},"4":{"name":"team5","hashed_password":"cq6rclb7ukh60l4q"}}
 }
 END2
 
 tee \$VAGRANT_HOME/HTTP-CTF/dashboard/static/js/firebase.init.js << END2
 // Initialize Firebase
 var config = {
-    apiKey: "AIzaSyDt1YJ0PuIc6Xz3KLm5Z6NJSgZPXH_ZXT0",
-    authDomain: "http-1661e.firebaseapp.com",
-    databaseURL: "https://http-1661e.firebaseio.com",
-    projectId: "http-1661e",
-    storageBucket: "http-1661e.appspot.com",
-    messagingSenderId: "584427206756"
+    apiKey: "AIzaSyAEa3nRkUUT5Q4qoiLF5fuClkKYqDL8l1Q",
+    authDomain: "cs408e-http.firebaseapp.com",
+    databaseURL: "https://cs408e-http.firebaseio.com",
+    projectId: "cs408e-http",
+    storageBucket: "cs408e-http.appspot.com",
+    messagingSenderId: "67045887531"
   };
 firebase.initializeApp(config);
 END2
@@ -87,15 +87,15 @@ END2
 tee \$VAGRANT_HOME/HTTP-CTF/database/config/firebaseConfig.json << END2
 {
   "type": "service_account",
-  "project_id": "http-1661e",
-  "private_key_id": "8932b53516590f13928ad4501177f953ed86d8ff",
-  "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQDBQTDKmR8YiPEd\nMM2K3nRBdjsydhwemrOcSfRsInX6YPe0qnoSuGoXRRmafCGk1rXg5MbOummuw3Ou\nPFp3pizHuLK55SqOP6LhjhGOF7vswQpKX+ncnuziLmft3IVMf04ICJuK+Y24Lt6m\n9OnlgnWOwqN24KGEpXTsfcl4/cKaKKZJhuJ2tMf0ChxHhR/EKFOFnN4/hRfTROiX\nBGK2RQSutIUDG2YT1idKWJCR3P/P2C6z0ZIp2qtUDrFwUBXPSnCY5PFAJLj9stFS\nhukXZZxlculzBCsNI7mri3kxVZvpgFhoMK1XvgfKx6lkhD5yoeK0ay6zv1WVpbY6\nEx6X+Z3JAgMBAAECggEABLA+J2ojK/NAO0EF+Y33pCdtcfuAJ49sAa6LInehulpx\neVWkoBvkf1jxhog8q40Qd6AWln5wBi8EeVLfmdUYnhtOuGMVNFiE9858gfX03Oka\n2hlSg9lWCKTk0quFud3W9iuu9LEPesCQrbGrKNfl3iSTdpS7omt+XHxEP9N940Nu\nl06FPobulpwpbBq5KUdaW+3L9GJB+Vk9fk4My5OZK8ImyLBW/GvQcVjvWQTF7b/n\n1qqzNLv7dYv5DxO477iVDrZlQlWlOEItkBydIMXkfUv7mfPiVIBR2y3BaBOF/0NU\nM4x1VBQOEVs2VI8I6Q1b5RNc9ubp3bnWVipce0RAMQKBgQD456NBtLibgWYbDRD/\nOsqq4S5su98fXDiU4lSLvoZNB9ymMP3wfXK38lt10AgQkPszzJc+6ibgdCwtT1fh\ne58vcOoAg8/R9oPdEq9r6OfoGAQkjIcFP36UMmsW6qcPb1qk5CXdl3qAfKtGKdov\nMPm0Z5kSB8cM4Jq8u/fTEFCaEQKBgQDGw3NGUNw27v6RaR+MDsfpA7l8gfhxuqUQ\nbgDD0hemNjz/kK2Miwkp2uWBVAzTvGXHIibGtDXG/gE2PRZyBDGgadx5ROJJq09R\nsjfNP7dBwNWw/g1qHfcYyzeYjpkD4yycPGZ//cMBVPLxvzYJT9l/tUggUaLB90XM\n6ZHEtcJQOQKBgQCAgL6VjqL8jdrUPnO3AXhernEUbA4ul6MSoqL5nryhilyNZJ/G\nCE4wz6wOb/+p5d/7BzZSofq+SNwB7IO3guZVTNtStrqVcMr1WM8+S/lmpveEkxSm\nTfigtEL7itwDKP9IQA2YkLsLGNIoIDVnxcY1MIMjfuZyLIdWx0iULEkgsQKBgEKt\nVGFVRNqEyA89paCVYMixQCsdapu0UiU10bnfI6HVdhcBkMah6ZwJ243MBVWOUNJB\niaKE17l+gieQzJPJBmDawmRIEGVIO56bW47V/gLnzofGYfekP70ohuj9hSVBszhi\nJrxvA1jSB4HDdDwhdkQaKyz0VdYdvmOkQwsrMhk5AoGACYqRxCAomKgTV++1gfug\nhq5DBVwZJSQ01EgrasDO+ZojlTskYgBePwz5exO7KhUyIgg/alVshMMxr/BH2jeM\ngZwlqvbtuVCOLgXaT84tRgAwHu8ssgo1silEBOu/FU3jSqpVxyfFcwtmSNCk+otR\nWS5kk3Buf1ywotBFRmko4ug=\n-----END PRIVATE KEY-----\n",
-  "client_email": "firebase-adminsdk-ryz10@http-1661e.iam.gserviceaccount.com",
-  "client_id": "112948171011989343622",
+  "project_id": "cs408e-http",
+  "private_key_id": "fcec7bee57a5b4aeabf611a44ff0093e8639c09f",
+  "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQC+dGjY03RU62c0\n8OR2+9t7x7R+8If1D3GCjyrIZRgvMAKId0e2/8dTblP6TIBO0liCiiuWJKc2l143\nd8BH5VOHVWeefdkqQpyd+1H/VyBsEVCQNEFH+ZUkaARqzk+qsuUx0pVmSotblQcF\n4N+dEaad3aRIS8OREraE7BzafxbuCUHDSo0XDmW1nGExQ+oXswTcMix5H+GbeDTw\nRxrzDocqHhNs5qI7EtC8X2nUHp74D6EiNJ5c35r+fAcXzUFvY/HyXk8xxsMgHGyD\nVsVLJX+aqbTGe79Q/161atXlKuAyyfyBTia1wkRbRtchTRGw/YUkhfJ9t9ohW/Rk\nNUhFFxavAgMBAAECggEAERcPZ3wwtq65qqRTzqIoCPybzj5APPXJ+O4RkpI7xX7j\nOKKszUI40obrwzdFK42FgrqU/FqapJ6YRkBD1qUV8JFx2bovmAgKghn7ZX8dz4S4\nPCXBloynXPEhNa0AtMUWevjguMvx+GGwPnn2JuPewHtokqeLINlTMSyiJDB+UBVd\n3n8s8mZ6jOwr6w/kbloo5qCKv25zFP0lW562u9FzNBAQa8pONfOEYmZJ4kLKKk3s\nC5FOZwMBLCizT/GIpDVKXh1vrjwoiCmuHF0SUZIYre6oBizGY4pKvjc0SGLhRk6a\nlN+rn3GpR+AkplI27G+MynS1TbQYujM0B2f1I0LXxQKBgQDqK0/oBxuWuzE222LE\nfKIWi+zRAJ35/tIFB0zAjSLnifJTOJipJEJSEQ/AJLpdcKMNk5pmhEUSHrgjd1uX\nB1t/JTaZkO9JYxNGO7fKMeHcfX3KajFFo8/h8r11bEjYCpuV7lAbqtqeaINfWsen\njcjKa1//280hqe2maUna3ZNybQKBgQDQNc6SZC9GioMKvHKFEP0C+J2x5Vsh37iM\nq7vOGlAqTvOjxS5kG1TuZyuFAgVYuEUHDZqiDGrJkaOvPsCIh05QEyeWeWbzPe4G\nYHC+JDkoMeoA+Z8gVFSSJgK2cUW7sYU2zo+nweIjlcqXZS07PZZJnUJeoU311wVx\nQA4a9+pcCwKBgBBErAt5mvAQ1y1xdl4iLnyUggJPIaqBioOw6yMhawBc9AnWD70I\neNcMumRl6NCfB6Tk0UA4BjhpC+/i1ekbKL8fjbiDeixGBsvFUFJqvr87tKaAVCX7\naHDRFVp4CGOB8ScsZEJXz1Jb/mx8eQxp7D4r0YKL+adSD+GUxP3AkQLNAoGAGLKp\nRFg5+2Ym0Tk1ZG91c8rF+fo46zW8kIM2jeY1KC+hWwXi0OElG/qFAQwSUknZdlK4\nywwqBqsN/ZW2BPv34CvFgGX6PlRUTdFWzldBqHDzTxZNN+qI9aUooTeii/cs1CmA\nhOfMyWfVVkwcJ3DQyP6shskflE/jR2HPyocp960CgYEA3sAUO2u7y8PqiWXshBzg\nBylTxKWFLWVn7gqJPWNXUVR2x0VAuH+nFNX4tLCrpyeNOhziS+vt2fIpvoX0u0ld\n58+CVZMIMAlRdn1gnGzTXa3PAfafW9jBWP+eoIrDH6/sE0+KF+9nUQNtaYaRfifD\nnbbfjkJipOTxXy9/kBfVJdQ=\n-----END PRIVATE KEY-----\n",
+  "client_email": "firebase-adminsdk-8cfa6@cs408e-http.iam.gserviceaccount.com",
+  "client_id": "113214710667042764687",
   "auth_uri": "https://accounts.google.com/o/oauth2/auth",
   "token_uri": "https://accounts.google.com/o/oauth2/token",
   "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-ryz10%40http-1661e.iam.gserviceaccount.com"
+  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-8cfa6%40cs408e-http.iam.gserviceaccount.com"
 }
 
 END2
@@ -103,19 +103,21 @@ END2
 tee \$VAGRANT_HOME/HTTP-CTF/database/settings.py << END2
 DEBUG = True
 MYSQL_DATABASE_USER = "root"
-MYSQL_DATABASE_PASSWORD = "http8804"
+MYSQL_DATABASE_INIT_PASSWORD = "http8804"
+MYSQL_DATABASE_PASSWORD = "ksj7b45fzh1ntws4"
 MYSQL_DATABASE_DB = "ctf"
 DOCKER_DISTRIBUTION_SERVER = "localhost:5000"
 DOCKER_DISTRIBUTION_USER = "root"
 DOCKER_DISTRIBUTION_PASS = "http8804"
 DOCKER_DISTRIBUTION_EMAIL = "hobincar@kaist.ac.kr"
 REMOTE_DOCKER_DAEMON_PORT = 2375
-TICK_TIME_IN_SECONDS=600
+TICK_TIME_IN_SECONDS = 600
+DB_SECRET = "kkosraclkkd4kb2g"
 END2
 
 tee \$VAGRANT_HOME/HTTP-CTF/scorebot/settings.py << END2
 DB_HOST = '127.0.0.1:4000'
-DB_SECRET = 'YOUKNOWSOMETHINGYOUSUCK'
+DB_SECRET = 'kkosraclkkd4kb2g'
 END2
 sudo tee /etc/gitlab/gitlab.rb << END2
 external_url 'http://localhost:5001'
@@ -128,7 +130,7 @@ registry['notifications'] = [
     'threshold' => 5,
     'backoff' => '2s',
     'headers' => {
-      'secret' => ['YOUKNOWSOMETHINGYOUSUCK']
+      'secret' => ['kkosraclkkd4kb2g']
     }
   }
 ]
@@ -139,7 +141,7 @@ sudo python create_containers.py -sl ../services -c example.json
 sudo python create_flag_dirs.py -c example.json
 
 cd \$VAGRANT_HOME/HTTP-CTF/database
-sudo python reset_db.py ../container-creator/output/Awesome CTF/initial_db_state.json
+sudo python reset_db.py ../container-creator/output/CTF-example/initial_db_state.json
 sudo gitlab-ctl reconfigure
 
 cd \$VAGRANT_HOME/HTTP-CTF/gitlab
